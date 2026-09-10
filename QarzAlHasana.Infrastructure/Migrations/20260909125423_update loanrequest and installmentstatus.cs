45822921f78801a49e81e3e38c311332a876bc01
Mohammadhossein Faraji <mhfaraji13@gmail.com>
2026-09-10 13:14:04 +0330

    chore: add migration for penalty fields and ignore .DS_Store

---
+++ QarzAlHasana.Infrastructure/Migrations/20260909125423_update loanrequest and installmentstatus.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QarzAlHasana.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateloanrequestandinstallmentstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PaidAmount",
+                table: "Installments",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PenaltyAmount",
+                table: "Installments",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaidAmount",
+                table: "Installments");
+
+            migrationBuilder.DropColumn(
+                name: "PenaltyAmount",
+                table: "Installments");
+        }
+    }
+}
